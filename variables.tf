a17187a3ffe8f9d50f91572284cd044de0b2175c
Triovians <connect@triovative.com>
2025-10-29 06:27:34 +0530

    Modify variables for AWS configuration
    Updated AWS region and added access/secret key variables.

--- variables.tf
+++ variables.tf
@@ -1,39 +1,46 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
-  default     = "us-east-1"
+  type        = string
+  default     = "ap-south-1"
 }
 
-variable "project_name" {
-  description = "Name prefix for all resources"
-  default     = "myawsproject"
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+  description = "CIDR for the VPC"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
-  description = "CIDR block for subnet"
+  description = "CIDR for the subnet"
+  type        = string
   default     = "10.0.1.0/24"
-}
-
-variable "availability_zone" {
-  description = "Availability zone"
-  default     = "us-east-1a"
-}
-
-variable "ami_id" {
-  description = "AMI ID for EC2 instance"
-  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
+  type        = string
   default     = "t2.micro"
 }
 
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  type        = string
+  default     = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 in ap-south-1
+}
+
 variable "key_name" {
-  description = "SSH key pair name"
-  default     = ""
+  description = "EC2 key pair name"
+  type        = string
 }
