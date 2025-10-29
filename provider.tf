5cf593b434ee0c2087098bbb3bd2b0c3268b25f6
Triovians <connect@triovative.com>
2025-10-29 06:27:08 +0530

    Add Terraform provider configuration for AWS

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {} # (optional) You can configure remote state later
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
