2da2b9112e4cdca98c4cab269708b45ecdd2670f
Triovians <connect@triovative.com>
2025-11-01 20:10:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,15 +81,38 @@ resource "local_file" "private_key" {
   filename = "${path.module}/tf-ec2-key.pem"
 }
 
+# -----------------------------
+# Get the latest Ubuntu 22.04 LTS AMI
+# -----------------------------
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"] # Canonical (official Ubuntu account)
+}
+
+output "ubuntu_ami_id" {
+  value =data.aws_ami.ubuntu
+}
+
 # EC2 Instance
 resource "aws_instance" "vm" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.vm_sg.id]
   key_name               = aws_key_pair.deployer_key.key_name
 
   tags = {
-    Name = "free-tier-vm"
+    Name = "ubuntu-server"
   }
 }
