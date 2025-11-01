2da534a51f9017be276657c9b0fd67876c3b24e1
Triovians <connect@triovative.com>
2025-11-01 19:44:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "aws_access_key" {
